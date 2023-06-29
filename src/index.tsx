5529b6a7fc5cb70a769fd568d0c5f18a0437d36f
Vecsel <116831920+Vecsel@users.noreply.github.com>
2023-06-29 11:06:48 +0300

    configured ccs module

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 
 import {render} from "react-dom";
-import {Counter} from "./components/Counter";
+import App from "./App";
 
 
 render(
-    <Counter/>,
+    <App/>,
     document.getElementById('root')
 )
