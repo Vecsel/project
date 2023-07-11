23e3d24966ac66ea3b39d80dbebd5f43fb43ef8d
Vecsel <116831920+Vecsel@users.noreply.github.com>
2023-07-11 10:02:01 +0300

    architecture added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React, {Suspense,} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss'
-import {MainPageAsync} from "./pages/MainPages/MainPage.async";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPages";
+
 
 
 const App = () => {
@@ -18,8 +19,8 @@ const App = () => {
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
 
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
 
                 </Routes>
             </Suspense>
