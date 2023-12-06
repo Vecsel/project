e1993a2386f44eca363939ec2ffa9ffeb35c8c92
Vecsel <116831920+Vecsel@users.noreply.github.com>
2023-12-06 16:53:08 +0300

    upgrade sidebar, add button themes

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { Sidebar } from './Sidebar';
 
 export default {
@@ -16,8 +17,12 @@ export default {
 const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
 
 export const Light = Template.bind({});
-Light.args = {};
+Light.args = {
+    children: 'Text',
+};
 
 export const Dark = Template.bind({});
-Dark.args = {};
+Dark.args = {
+    children: 'Text',
+};
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
