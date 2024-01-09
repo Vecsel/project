dc263ed274b0d44e8253b1fa6435737ab3590636
Vecsel <116831920+Vecsel@users.noreply.github.com>
2024-01-09 18:21:13 +0300

    add new input, add login from ui

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -10,7 +10,8 @@ interface ModalProps {
     className?: string;
     children?: ReactNode;
     isOpen?: boolean;
-    onClose?: () => void
+    onClose?: () => void;
+    lazy?: boolean;
 }
 
 const ANIMATION_DELAY = 300;
@@ -21,11 +22,19 @@ export const Modal = (props:ModalProps) => {
         children,
         isOpen,
         onClose,
+        lazy,
     } = props;
 
     const [isClosing, setIsClosing] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
     const { theme } = useTheme();
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+        }
+    }, [isOpen]);
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -62,6 +71,9 @@ export const Modal = (props:ModalProps) => {
         [cls.isClosing]: isClosing,
     };
 
+    if (lazy && !isMounted) {
+        return null;
+    }
     return (
         <Portal>
             <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { useCallback, useState } from 'react';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,8 +14,12 @@ export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
 
     return (
@@ -22,14 +27,14 @@ export const Navbar = ({ className }: NavbarProps) => {
             <Button
                 theme={ButtonTheme.CLEAR_INVERTED}
                 className={cls.links}
-                onClick={onToggleModal}
+                onClick={onShowModal}
             >
                 {t('Войти')}
             </Button>
-            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                {/* eslint-disable-next-line max-len */}
-                {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, nihil voluptas? Ab eum molestiae natus nisi quis. Aliquid facere labore magni nesciunt non possimus provident ratione reiciendis. Accusantium autem eveniet itaque maxime neque nobis odit optio qui quidem voluptate. Cum dicta eius esse exercitationem impedit incidunt magni nemo nesciunt quae tenetur? Aspernatur est nisi nulla praesentium temporibus? Amet asperiores cumque dicta distinctio eveniet facilis inventore pariatur sed tenetur? Architecto consequuntur deleniti dolor doloribus dolorum, ducimus eligendi, error et illo incidunt ipsa iste itaque laborum nemo, officiis omnis pariatur perspiciatis placeat quaerat quis reprehenderit sequi sint suscipit unde vero! Iure, repudiandae.')}
-            </Modal>
+            <LoginModal
+                isOpen={isAuthModal}
+                onClose={onCloseModal}
+            />
         </div>
     );
 };
